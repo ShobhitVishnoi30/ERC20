94f7e21fbee6e96abe5ca018f93cd12c212166a7
shobhitvishnoi30 <vishnoishobhit201@gmail.com>
2022-09-18 11:07:06 +0530

    test:added transferFroma and approve function

--- test/sample-test.js
+++ test/sample-test.js
@@ -14,12 +14,11 @@ async function Testing() {
   const testERC20 = await TestERC20.deploy("testERC20", "TERC");
   await testERC20.deployed();
   describe("TestERC20", async function () {
-    console.log(testERC20.address);
     it("Should return the correct name and symbol", async function () {
       expect(await testERC20.name()).to.equal("testERC20");
 
       expect(await testERC20.symbol()).to.equal("TERC");
-    }).timeout("30s");
+    });
 
     it("only owner can mint tokens", async function () {
       await expect(await testERC20.owner()).to.equal(user1.address);
@@ -27,7 +26,7 @@ async function Testing() {
       await expect(
         testERC20.connect(user2).mint(user2.address, 100)
       ).to.be.revertedWith("Ownable: caller is not the owner");
-    }).timeout("30s");
+    });
 
     it("owner can mint tokens", async function () {
       await expect(await testERC20.owner()).to.equal(user1.address);
@@ -39,7 +38,7 @@ async function Testing() {
       expect((await testERC20.balanceOf(user1.address)).toString()).to.equal(
         "1000000000000"
       );
-    }).timeout("30s");
+    });
 
     it("user can transfer tokens", async function () {
       expect((await testERC20.balanceOf(user1.address)).toString()).to.equal(
@@ -53,7 +52,7 @@ async function Testing() {
         "999999999900"
       );
       expect(await testERC20.balanceOf(user2.address)).to.equal("100");
-    }).timeout("30s");
+    });
 
     it("user can not transfer tokens more than balance", async function () {
       expect((await testERC20.balanceOf(user1.address)).toString()).to.equal(
@@ -63,8 +62,42 @@ async function Testing() {
       await expect(
         testERC20.connect(user1).transfer(user2.address, 999999999901)
       ).to.be.revertedWith("ERC20: transfer amount exceeds balance");
-    }).timeout("30s");
-  }).timeout("500s");
+    });
+
+    it("user should be able to approve users to transfer token", async function () {
+      expect(await testERC20.allowance(user1.address, user2.address)).to.equal(
+        "0"
+      );
+
+      await testERC20.connect(user1).approve(user2.address, 100);
+
+      expect(await testERC20.allowance(user1.address, user2.address)).to.equal(
+        "100"
+      );
+    });
+
+    it("user should be able to transfer tokens on the behalf of the token owner who has approved user", async function () {
+      expect(await testERC20.balanceOf(user3.address)).to.equal("0");
+
+      expect(await testERC20.balanceOf(user1.address)).to.equal("999999999900");
+      await testERC20
+        .connect(user2)
+        .transferFrom(user1.address, user3.address, 50);
+
+      expect(await testERC20.balanceOf(user1.address)).to.equal("999999999850");
+
+      expect(await testERC20.balanceOf(user3.address)).to.equal("50");
+    });
+
+    it("user should not be able to transfer more tokens than allowed", async function () {
+      expect(await testERC20.allowance(user1.address, user2.address)).to.equal(
+        "50"
+      );
+      await expect(
+        testERC20.connect(user2).transferFrom(user1.address, user3.address, 51)
+      ).to.be.revertedWith("ERC20: insufficient allowance");
+    });
+  });
 }
 
 describe("Setup & Test Contracts", async function () {
